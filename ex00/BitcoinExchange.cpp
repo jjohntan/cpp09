2e62065027cbaf706588923d285ed53eea130745
jjohntan <tanjekjohn@gmail.com>
2025-10-14 17:12:02 +0800

    add close file and add use getline to separate strings in loadDataBase()

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 13:51:50 by jetan             #+#    #+#             */
-/*   Updated: 2025/10/13 19:22:02 by jetan            ###   ########.fr       */
+/*   Updated: 2025/10/14 17:07:20 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 void BitcoinExchange::loadDataBase(const std::string &filename)
 {
-	std::ifstream file(filename.c_str());
+	std::ifstream ifs(filename.c_str());// input file stream
 	
-	if (!file.is_open())
+	if (!ifs.is_open())
 	{
 		std::cerr << "Error: could not open file." << std::endl;
 		return ;
 	}
 	
 	std::string line;
-	std::getline(file, line);// skip the first line
-	while (std::getline(file, line))
+	std::getline(ifs, line);// skip the first line
+	while (std::getline(ifs, line))
 	{
-		std::cout << line << std::endl;
+		// std::cout << line << std::endl;
+		std::string date, value;
+		std::getline(ifs, date, ',');
+		// std::cout << "date: " << date << std::endl;
+		std::getline(ifs, value);
+		// std::cout << "value: " << value << std::endl;
 	}
+	ifs.close();
 }
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
