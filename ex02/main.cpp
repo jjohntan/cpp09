82b57dffd9ccbcc166202b9e83b4b3a75efd24d2
jjohntan <tanjekjohn@gmail.com>
2025-10-22 14:13:16 +0800

    fix check if is non char issue

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:51:52 by jetan             #+#    #+#             */
-/*   Updated: 2025/10/21 18:31:12 by jetan            ###   ########.fr       */
+/*   Updated: 2025/10/22 14:11:07 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,17 @@ int main(int ac, char **av)
 	}
 	for (int i = 1; i < ac; i++)
 	{
+		std::string arg = av[i];
+		for (unsigned int j = 0; j < arg.size(); j++)
+		{
+			if (!std::isdigit(arg[j]))// check if is non char
+			{
+				std::cerr << "Error" << std::endl;
+				return 1;
+			}
+		}
 		int n = std::atoi(av[i]);
 		if (n < 0)// check if non positive int
-		{
-			std::cerr << "Error" << std::endl;
-			return 1;
-		}
-		if (!std::isdigit(n))// check if is non char
 		{
 			std::cerr << "Error" << std::endl;
 			return 1;
